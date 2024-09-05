a12488278ac19de89bc92a20f85e084a7dd0ebbb
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-09-05 20:40:23 +0530

    initialize user.php

---
+++ user.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container my-5">
+    <form method="post">
+  <div class="mb-3">
+    <label>Name</label>
+    <input type="text" class="form-control" name="name" placeholder="Enter your name">
+   
+  </div>
+
+  <div class="mb-3">
+    <label>Email</label>
+    <input type="text" class="form-control" name="email" placeholder="Enter your Email">
+   
+  </div>
+
+  <div class="mb-3">
+    <label>Phone</label>
+    <input type="text" class="form-control" name="Phone" placeholder="Enter your phone">
+   
+  </div>
+
+  <div class="mb-3">
+    <label>Address</label>
+    <input type="text" class="form-control" name="Address" placeholder="Enter your Address">
+   
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+    
+  </body>
+</html>
